504638b2cab1e68edf3195d94c7f5ae23cfe4ab8
Danny Flores <djoeflores@gmail.com>
2017-11-21 19:12:53 -0600

    Removed some lines from index.php

--- index.php
+++ index.php
@@ -12,13 +12,10 @@
     
     //require 'Controllers/' . $url . '.php';
     $controller = new $url[0];
-    
-    //$controller2 = new Help();
-        
+          
     if (isset($url[1])) {
         $controller->{$url[1]}();
     }
     
-    //$controller2->Aid();
 ?>
 
